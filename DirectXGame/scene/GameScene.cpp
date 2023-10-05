6e816cea2d10600b50d3be78b037c119e6ac110a
satoisamu2720 <115054959+satoisamu2720@users.noreply.github.com>
2023-10-05 12:32:18 +0900

    完成

--- DirectXGame/scene/GameScene.cpp
+++ DirectXGame/scene/GameScene.cpp
@@ -1,21 +1,149 @@
 #include "GameScene.h"
+#include "MathUtilityForText.h"
 #include "TextureManager.h"
 #include <cassert>
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {}
+GameScene::~GameScene() { 
+	delete modelPlayer_; 
+	delete modelBeam_;
+}
 
 void GameScene::Initialize() {
 
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
+	// ビュープロジェクションの初期化
+	viewProjection_.translation_.y = 2;
+	viewProjection_.translation_.z = -6;
+	viewProjection_.Initialize();
+
+	// プレイヤー
+	textureHandlePlayer_ = TextureManager::Load("player.png");
+	modelPlayer_ = Model::Create();
+	worldTransformPlayer_.scale_ = {0.2f, 0.2f, 0.2f};
+	worldTransformPlayer_.Initialize();
+	// ビーム
+	textureHandleBeam_ = TextureManager::Load("beam.png");
+	modelBeam_ = Model::Create();
+
+	for (int b = 0; b < 10; b++) {
+		worldTransformBeam_[b].scale_ = {0.3f, 0.3f, 0.3f};
+		worldTransformBeam_[b].Initialize();
+	}
+}
+void GameScene::GamePlayUpdete() { 
+	PlayerUpdate(); 
+	BeamUpdate();
+}
+void GameScene::Update() { 
+	GamePlayUpdete(); 
 }
 
-void GameScene::Update() {}
+// プレイヤー更新処理
+void GameScene::PlayerUpdate() {
 
-void GameScene::Draw() {
+	// 移動
+	worldTransformPlayer_.translation_.x += playerSpeed_;
+	// 左へ移動
+	if (worldTransformPlayer_.translation_.x > 4) {
+		playerSpeed_ = -0.05f;
+	}
+	// 右へ移動
+	if (worldTransformPlayer_.translation_.x < -4) {
+		playerSpeed_ = 0.05f;
+	}
+
+	// 変換行列を更新
+	worldTransformPlayer_.matWorld_ = MakeAffineMatrix(
+	    worldTransformPlayer_.scale_, worldTransformPlayer_.rotation_,
+	    worldTransformPlayer_.translation_);
+	// 変換行列を定数バッファに転送
+	worldTransformPlayer_.TransferMatrix();
+
+	if (playerTimer_ > 0) {
+		playerTimer_ -= 1;
+	}
+}
+
+// ビーム更新処理
+void GameScene::BeamUpdate() {
+	BeamMove();
+	BeamBorn();
+	// 変換行列を更新
+	for (int i = 0; i < 10; i++) {
+		if (beamFlag_[i] == 1)
+
+			worldTransformBeam_[i].matWorld_ = MakeAffineMatrix(
+			    worldTransformBeam_[i].scale_, worldTransformBeam_[i].rotation_,
+			    worldTransformBeam_[i].translation_);
+		// 変換行列を定数バッファに転送
+		worldTransformBeam_[i].TransferMatrix();
+	}
+}
+
+// ビーム移動
+void GameScene::BeamMove() {
+
+	for (int i = 0; i < 10; i++) {
+		if (beamFlag_[i] == 1) {
+			worldTransformBeam_[i].translation_.z += 0.5f;
+			worldTransformBeam_[i].translation_.y += 0.1f;
+			// 回転　
+			worldTransformBeam_[i].rotation_.x += 0.1f;
+		}
+	}
+}
+
+// ビーム
+void GameScene::BeamBorn() {
+	// スペースキーで発射
+	for (int b = 0; b < 10; b++) {
+		if (beamTimer_ == 0) {
+
+			if (input_->PushKey(DIK_SPACE) && beamFlag_[b] == 0) {
+				worldTransformBeam_[b].translation_.x = worldTransformPlayer_.translation_.x;
+				worldTransformBeam_[b].translation_.y = worldTransformPlayer_.translation_.y;
+				worldTransformBeam_[b].translation_.z = worldTransformPlayer_.translation_.z;
+
+				beamTimer_ = 1;
+				beamFlag_[b] = 1;
+				break;
+			}
+
+		} else {
+
+			if (beamTimer_ >= 1) {
+				beamTimer_++;
+
+				if (beamTimer_ > 30) {
+					beamTimer_ = 0;
+				}
+				break;
+			}
+		}
+
+		// // 40以上で削除
+		if (worldTransformBeam_[b].translation_.z > 40) {
+			beamFlag_[b] = 0;
+			beamTimer_ = 0;
+		}
+	}
+}
+
+void GameScene::GamePlayDraw3D() {
+	//プレイヤー
+	modelPlayer_->Draw(worldTransformPlayer_, viewProjection_, textureHandlePlayer_);
+	//弾
+	for (int b = 0; b < 10; b++) {
+		if (beamFlag_[b] == 1) {
+			modelBeam_->Draw(worldTransformBeam_[b], viewProjection_, textureHandleBeam_);
+		}
+	}
+}
+	void GameScene::Draw() {
 
 	// コマンドリストの取得
 	ID3D12GraphicsCommandList* commandList = dxCommon_->GetCommandList();
@@ -40,6 +168,7 @@ void GameScene::Draw() {
 
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
+	 GamePlayDraw3D();
 	/// </summary>
 
 	// 3Dオブジェクト描画後処理
--- DirectXGame/scene/GameScene.h
+++ DirectXGame/scene/GameScene.h
@@ -8,18 +8,43 @@
 #include "Sprite.h"
 #include "ViewProjection.h"
 #include "WorldTransform.h"
+#include "DebugCamera.h"
 
 /// <summary>
 /// ゲームシーン
 /// </summary>
 class GameScene {
 
+	// プレイヤー
+	uint32_t textureHandlePlayer_ = 0;
+	Model* modelPlayer_ = nullptr;
+	WorldTransform worldTransformPlayer_;
+
+	int playerLife_ = 3;
+	int playerTimer_ = 0;
+	float playerSpeed_ = 0.05f;
+	// プレイヤー更新
+	void PlayerUpdate();
+
+	// ビーム
+	uint32_t textureHandleBeam_ = 0;
+	Model* modelBeam_ = {};
+	WorldTransform worldTransformBeam_[10];
+
+	void BeamMove();
+	void BeamUpdate();
+	void BeamBorn();
+
+	int beamFlag_[10] = {};
+	int beamTimer_ = 0;
+
 public: // メンバ関数
 	/// <summary>
 	/// コンストクラタ
 	/// </summary>
 	GameScene();
-
+	// ビュープロジェクション(共通)
+	ViewProjection viewProjection_;
 	/// <summary>
 	/// デストラクタ
 	/// </summary>
@@ -34,17 +59,34 @@ public: // メンバ関数
 	/// 毎フレーム処理
 	/// </summary>
 	void Update();
-
+	void GamePlayUpdete();
+	
 	/// <summary>
 	/// 描画
 	/// </summary>
 	void Draw();
+	void GamePlayDraw3D();
 
 private: // メンバ変数
 	DirectXCommon* dxCommon_ = nullptr;
 	Input* input_ = nullptr;
 	Audio* audio_ = nullptr;
 
+	/*uint32_t textureHandle_ = 0;
+	Sprite* sprite_ = nullptr;
+
+	Model* model_ = nullptr;
+	WorldTransform worldTransform_;
+	ViewProjection viewProjection_;
+
+	uint32_t soundDataHandle_ = 0;
+	uint32_t voiceHandle_ = 0;
+
+	float inputFloat3[3] = {0, 0, 0};
+
+	DebugCamera* debugCamera_ = nullptr;
+
+	bool isDebugcameraActive_ = false;*/
 	/// <summary>
 	/// ゲームシーン用
 	/// </summary>
